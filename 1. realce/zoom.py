bfc209aba3ccfcc6bc75d50978ce492482d4a150
Arthur Flôr <arthurflor23@gmail.com>
2018-08-17 20:27:01 -0300

    create function zoom()

--- 1. realce/zoom.py
+++ 1. realce/zoom.py
@@ -1,43 +1,46 @@
 from PIL import Image
 import numpy
+import os
 
-load_path = "./zoom/Zoom_in_(1).jpg"
-save_path = "./"
+def zoom(img, n_width, n_height):
+    path = "./zoom/" + img
+    save_path = path + "result/" + img
 
-im = Image.open(load_path)
-arr = numpy.array(im)
+    im = Image.open(path, "r")
+    arr = numpy.array(im)
 
-height = len(arr)
-width = len(arr[0])
+    width = len(arr[0])
+    height = len(arr)
+    n_arr = numpy.zeros((n_height, n_width) , dtype=numpy.uint8)
 
-n_height = 480
-n_width = 360
+    if (n_width > width):
+        f_width = int(numpy.ceil((n_width / width)))
+    else:
+        f_width = 1
 
-f_height = int(n_height / height) 
-f_width = int(n_width / width)
+    if (n_height > height):
+        f_height = int(numpy.ceil((n_height / height)))
+    else:
+        f_height = 1
 
-dec_height = (n_height / height) - f_height
-dec_width = (n_width / width) - f_width
+    for y in range(len(arr)):
+        new_y = (y * f_height)
 
-print(f_height)
-print(dec_height)
-print()
-print(f_width)
-print(dec_width)
+        for x in range(len(arr[y])):
+            new_x = (x * f_width)
 
-new_arr = numpy.zeros((n_height, n_width))
+            for temp_y in range(f_height):
+                for temp_x in range(f_width):
+                    if (new_y + temp_y < n_height) and (new_x + temp_x < n_width):
+                        n_arr[(new_y + temp_y), (new_x + temp_x)] = arr[y, x]
 
-# rgb_im = im.convert('RGB')
-# arr = array(rgb_im)
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    Image.fromarray(n_arr).save(save_path)
 
-for y in range(len(arr)):
-    new_y = int(y * f_height)
+# zoom("Zoom_in_(1).jpg", 360, 480)
+# zoom("Zoom_in_(2).jpg", 2592, 1456)
+# zoom("Zoom_in_(3).jpg", 720, 990)
 
-    for x in range(len(arr[y])):
-        new_x = int(x * f_width)
-
-        for temp_y in range(f_height):
-            for temp_x in range(f_width):
-                new_arr[(new_y+temp_y), (new_x+temp_x)] = arr[y, x]
-    
-Image.fromarray(new_arr).show()
+# zoom("Zoom_out_(1).jpg", 271, 120)
+zoom("Zoom_out_(2).jpg", 317, 500)
+# zoom("Zoom_out_(3).jpg", 174, 500)
