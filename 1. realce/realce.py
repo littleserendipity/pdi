4b47192d048aca4ae1150668126ccbcf850bd7a7
Arthur Flôr <arthurflor23@gmail.com>
2018-08-18 13:49:22 -0300

    realce

--- 1. realce/realce.py
+++ 1. realce/realce.py
@@ -6,7 +6,7 @@ import os
 def getMinMax(arr):
     return [min(arr[arr != 0]), max(arr)]
 
-def histograma(arr):
+def histogram(arr):
     h_arr = numpy.zeros((256), dtype=numpy.uint8)
 
     for x in range(len(arr)):
@@ -18,30 +18,22 @@ def histograma(arr):
     print("\nMin e Max (min > 0): " + str(getMinMax(h_arr)) + "\n")
     return h_arr
 
-def linear(min, max, x):
+def no_linear(min, max, x, exp):
     a = 255/(max - min)
-    b = (-1 * a * min)
-    return (a*x) + b
+    return int(a * (x ** exp))
 
-def no_linear(min, max, x):
-    a = 255/(max - min)
-    return int(a * (x ** (1/2)))
-
-def realce(option, img):
+def realce(img, exp):
     path = "realce"
-    save_path = os.path.join(path, "result")
+    save_path = os.path.join(path, "result", img)
 
     im = Image.open(os.path.join(path, img), "r")
     l_pixel = list(im.getdata())
 
-    h_arr = histograma(l_pixel)
+    h_arr = histogram(l_pixel)
     p_min_max = getMinMax(h_arr)
 
     for x in range(len(l_pixel)):
-        if (option == "c"):
-            pixel = linear(p_min_max[0], p_min_max[1], l_pixel[x])
-        elif (option == "e"):
-            pixel = no_linear(p_min_max[0], p_min_max[1], l_pixel[x])
+        pixel = no_linear(p_min_max[0], p_min_max[1], l_pixel[x], exp)
 
         if (pixel < p_min_max[0]):
             l_pixel[x] = p_min_max[0]
@@ -53,19 +45,17 @@ def realce(option, img):
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     im2 = Image.new(im.mode, im.size)
     im2.putdata(l_pixel)
-    im2.show()
-    # im2.save(save_path)
+    # im2.show()
+    im2.save(save_path)
 
 begin = time.time()
+realce("Clarear_(1).jpg", 1.5)
+realce("Clarear_(2).jpg", 1.5)
+realce("Clarear_(3).jpg", 1.5)
 
-# realce("c", "Clarear_(1).jpg")
-realce("c", "Clarear_(2).jpg")
-# realce("c", "Clarear_(3).jpg")
-
-# realce("e", "Escurecer_(1).jpg")
-# realce("e", "Escurecer_(2).jpg")
-# realce("e", "Escurecer_(3).jpg")
-
+realce("Escurecer_(1).jpg", 0.75)
+realce("Escurecer_(2).jpg", 0.75)
+realce("Escurecer_(3).jpg", 0.75)
 end = time.time()
 
 print("Finalizado: " + str(round(end-begin, 2)) + "s\n")
