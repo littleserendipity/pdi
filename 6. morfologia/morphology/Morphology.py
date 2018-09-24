3750f589037ef7bbb0fe82c6cbc3adbff49e18a0
Arthur Flôr <arthurflor23@gmail.com>
2018-09-24 02:29:48 -0300

    question 2 question 3 (partial)

--- 6. morfologia/morphology/Morphology.py
+++ 6. morfologia/morphology/Morphology.py
@@ -29,3 +29,32 @@ class Morphology(object):
             img.setImg(temp)
 
         return img
+
+    def dilate(self, image, kernel=None, side=3):
+        if (kernel is None):
+            kernel = np.zeros((side,side))
+        return self.filterMorph(image, kernel, np.prod, np.multiply)
+    
+    def erode(self, image, kernel=None, side=3):
+        if (kernel is None):
+            kernel = np.zeros((side,side))
+        return self.filterMorph(image, kernel, np.sum, np.add)
+
+    def filterMorph(self, image, kernel, externalFunction, internalFunction):
+        im = copy.deepcopy(image)
+        m, n = kernel.shape
+        pad_h, pad_w = (m//2), (n//2)
+        H, W = image.arr.shape
+
+        img = np.ones((H + pad_h * 2, W + pad_w * 2)) * 128
+        new_img = np.ones((H + pad_h * 2, W + pad_w * 2))
+        img[pad_h:-pad_h, pad_w:-pad_w] = image.arr
+
+        for i in range(pad_h, H+pad_h):
+            for j in range(pad_w, W+pad_w):
+                current_window = img[i-pad_h:i+m-pad_h, j-pad_w:j+n-pad_w]
+                new_img[i,j] = externalFunction(internalFunction(current_window, kernel))
+
+        new_img[new_img > 0] = 1
+        im.setImg(new_img[pad_h:-pad_h,pad_w:-pad_w])
+        return im
--- 6. morfologia/morphology/main.py
+++ 6. morfologia/morphology/main.py
@@ -8,23 +8,41 @@ def main():
     s = seg.Thresholding()
     m = mp.Morphology()
 
-    img1a = s.otsu(im.Image("Image_(1a).png"))
-    img1b = s.otsu(im.Image("Image_(1b).png"))
+    # img1a = s.otsu(im.Image("Image_(1a).png"))
+    # img1b = s.otsu(im.Image("Image_(1b).png"))
 
-    imgOR = m.logicalOperator(img1a, img1b, "OR")
-    imgOR.save()
+    # imgOR = m.logicalOperator(img1a, img1b, "OR")
+    # imgOR.save()
 
-    imgAND = m.logicalOperator(img1a, img1b, "AND")
-    imgAND.save()
+    # imgAND = m.logicalOperator(img1a, img1b, "AND")
+    # imgAND.save()
 
-    imgXOR = m.logicalOperator(img1a, img1b, "XOR")
-    imgXOR.save()
+    # imgXOR = m.logicalOperator(img1a, img1b, "XOR")
+    # imgXOR.save()
 
-    imgNAND = m.logicalOperator(img1a, img1b, "NAND")
-    imgNAND.save()
+    # imgNAND = m.logicalOperator(img1a, img1b, "NAND")
+    # imgNAND.save()
 
     ### questão 2
-    
+    # img2a = s.otsu(im.Image("Image_(2a).jpg"))
+    # d_img2a = m.erode(m.erode(m.dilate(img2a)))
+    # d_img2a.save()
+
+    ### questão 3
+    img3a = s.otsu(im.Image("Image_(3a).jpg"))
+    # img3a = m.erode(img3a)
+
+    d_img3a = m.erode(img3a)
+    d_img3a.setImg((np.subtract(d_img3a.arr, img3a.arr)))
+
+    d_img3a = m.erode(d_img3a, kernel=np.array([[0,1,0],[1,1,1],[0,1,0]]))
+
+
+    img = m.logicalOperator(img3a, d_img3a, "OR")
+    img3a.show()
+    d_img3a.show()
+    img.show()
+
 
 if __name__ == '__main__':
     main()
